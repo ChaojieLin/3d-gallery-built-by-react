9c3e2798cb79f11fd9e72577ed1101c840fba177
chaojielin <u.s.chaojielim@gmail.com>
2018-04-07 13:08:43 -0400

    added volume control

--- src/components/progress.js
+++ src/components/progress.js
@@ -2,37 +2,22 @@ import React from 'react';
 import {jPlayer} from 'jplayer';
 import '../style/progress.less';
 
-let duration = null;
+
 class Progress extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            progress:0,
-            volume:0
         };
     };
-    componentDidMount(){
-        $('#player').bind($.jPlayer.event.timeupdate, function(e){
-            duration=e.jPlayer.status.duration;
-            this.setState({
-                progress: e.jPlayer.status.currentPercentAbsolute,
-                volume: e.jPlayer.status.volume
-            })
-        }.bind(this))
-    };
-    componentWillUnmount(){
-        $('#player').unbind($.jPlayer.event.timeupdate);
-    };
-    
     changeProgress(e){
         let progressBar=this.refs.progressBar;
         let progress=(e.clientX-progressBar.getBoundingClientRect().left)/progressBar.clientWidth;
-        $('#player').jPlayer('play', duration * progress);
+        this.props.onProgressChange&&this.props.onProgressChange(progress);
     };
     render(){
         return(
             <div className="components-progress" ref="progressBar" onClick={this.changeProgress.bind(this)}>
-                <div className="progress" style={{width:`${this.state.progress}%`,background:this.props.barColor}}></div>
+                <div className="progress" style={{width:`${this.props.progress}%`,background:this.props.barColor}}></div>
             </div>
         );
     };
--- src/pages/player.js
+++ src/pages/player.js
@@ -2,38 +2,83 @@ import React from 'react';
 import Progress from '../components/progress';
 import { MUSIC_LIST } from '../config/music_list';
 
+let duration = null;
 export default class Player extends React.Component{
     
     constructor(props){
         super(props);
         this.state = {
-            currentMusicItem : MUSIC_LIST[1]
+            progress:0,
+            volume:0,
+            isPlay:true,
+            currentMusicItem : MUSIC_LIST[0]
         };
     };
     componentDidMount(){
-        var music_file=this.state.currentMusicItem.file.toString();
+        let music_file=this.state.currentMusicItem.file.toString();
         $('#player').jPlayer({
             ready: function(){
                 $(this).jPlayer('setMedia',{
                     mp3:music_file
-                });
-                // .jPlayer('play')
+                }).jPlayer('play');
             },
             supplied: 'mp3',
             wmode:'window'
         });
-    }
+        $('#player').bind($.jPlayer.event.timeupdate, function(e){
+            duration=e.jPlayer.status.duration;
+            this.setState({
+                progress: e.jPlayer.status.currentPercentAbsolute,
+                volume: e.jPlayer.options.volume * 100
+            })
+        }.bind(this))
+    };
+    componentWillUnmount(){
+        $('#player').unbind($.jPlayer.event.timeupdate);
+    };
+    onProgressChange(progress){
+        $('#player').jPlayer(this.state.isPlay?'play':'pause', duration * progress);
+    };
+    onVolumeChange(progress){
+        $('#player').jPlayer('volume',progress);
+        //updating volume without refresh webpage
+        if(this.state.isPlay){
+            $('#player').jPlayer('play');
+        }else{
+            $('#player').jPlayer('play');
+            $('#player').jPlayer('pause');
+        }
+    };
+    play(){
+        if(this.state.isPlay){
+            $('#player').jPlayer('pause');
+        }else{
+            $('#player').jPlayer('play');
+        }
+        this.setState({
+            isPlay: !this.state.isPlay
+        })
+    };
     render(){
         return(
             <div className="pages-player">
                 <div className="progress-title">{this.state.currentMusicItem.title}</div>
                 <div className="progress-artist">{this.state.currentMusicItem.artist}</div>
                 <img className="progress-cover" src={this.state.currentMusicItem.cover} />
-                <Progress 
-                    currentMusic={this.state.currentMusicItem}
+                <Progress
+                    onProgressChange={this.onVolumeChange.bind(this)}
                     barColor='blue'
+                    progress={this.state.volume}
                 >
                 </Progress>
+                <Progress 
+                    onProgressChange={this.onProgressChange.bind(this)}
+                    progress={this.state.progress}
+                >
+                </Progress>
+                <div className={`${this.isPlay?'pause':'play'}`} onClick={this.play.bind(this)}>
+                    {this.isPlay?'pause':'play'}
+                </div>
             </div>
         )
     }
